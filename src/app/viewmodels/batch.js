6c0a78f1735aa9f5969fabe92254fd581e0279dd
hayden taylor <dev@hxtaylor.com>
2014-06-04 17:26:02 -0400

    functionality done, ready to fully refactor.

--- src/app/viewmodels/batch.js
+++ src/app/viewmodels/batch.js
@@ -1,6 +1,28 @@
-define(['durandal/app', 'knockout', 'models/emitter'], function (app, ko, Emitter) {
+define(['durandal/app', 'knockout','models/inputvalidator'], function (app, ko, InputValidator) {
 
   function Batch() {
+
+  	var self = this;
+
+  	var inputValidator = new InputValidator();
+
+  	self.finalOutput = ko.observableArray();
+
+  	self.rangeValue = ko.observable();
+
+  	this.nameit = ko.computed(function(){
+
+  		console.log(self.rangeValue());
+
+  		self.finalOutput.removeAll();
+
+  		for(var i = 0; i <= self.rangeValue(); i++) {
+  			console.log(i);
+  			self.finalOutput.push(inputValidator.inputCheck(i));
+
+  		}
+
+  	});
   }
 
   return new Batch();
--- src/app/viewmodels/test.js
+++ src/app/viewmodels/test.js
@@ -1,62 +1,16 @@
-define(['durandal/app', 'knockout', 'emitter_config','jquery','models/utilities'], function (app, ko, emitterConfig, $, Utilities) {
+define(['durandal/app', 'knockout','jquery','models/inputvalidator'], function (app, ko, $, InputValidator) {
 
   function Test() {
 
   	var self = this;
+
 	self.numInput = ko.observable();
 
-	console.log(self.numInput());
-	self.emitters = emitterConfig.emitters;
+	var inputValidator = new InputValidator();
 
 	self.numOutput = ko.computed(function() {
 
-		var utility = new Utilities();
-
-		var finalOutput = "";
-
-		var input = utility.trimWhiteSpaceFromString(self.numInput());
-
-		var outputHasBeenAssigned = false;
-
-		$.each(this.emitters(), function(index, value) {
-
-			console.log(ko.toJS(value));
-
-			if(value.isInputModEqualToZero(input)) {
-
-				finalOutput += value.output();
-
-				outputHasBeenAssigned = true;
-
-			}
-		});
-
-		if(!outputHasBeenAssigned) {
-
-			finalOutput = input;
-
-		}
-
-		if(isNaN(self.numInput())) {
-
-			finalOutput = "Not Valid Data";
-
-		}
-		else if(utility.isEqualToZero(input)) {
-
-			finalOutput = "0";
-
-		}
-		else if(utility.isLengthLessThanOne(input)) {
-
-			finalOutput = "" + self.numInput();
-
-		}
-		else if(utility.isEqualToASpace(input)) {
-
-			finalOutput = "Not Valid Data";
-
-		}
+		var finalOutput = inputValidator.inputCheck(self.numInput());
 
 		return finalOutput;
 
